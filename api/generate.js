07f8230b8ca3705ee3fda101dbdc5795340c09e5
Shrinivas <shrinivas@ripplemedia.co>
2025-07-17 14:45:07 +0530

    Updated generate file

--- api/generate.js
+++ api/generate.js
@@ -1,5 +1,5 @@
 // /api/generate.js
-
+console.log("✅ /api/generate function triggered");
 export default async function handler(req, res) {
   if (req.method !== "POST") {
     return res.status(405).json({ error: "Method Not Allowed" });
